673580cc571aa0e6647d9df6de9063b41be39e75
Logan Woolf <logan.woolf@arcticleaf.io>
2024-11-11 03:56:53 -0700

    Show some data from pb

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	export let data;
+</script>
+
+<h1>Hello</h1>
+
+<h2>Posts</h2>
+<ul>
+	{#each data.posts.items as post}
+		<li>
+			<article>
+				<header>
+					<h3>{post.title}</h3>
+				</header>
+				<main>
+					<p>{@html post.content}</p>
+				</main>
+				<footer>
+					<p>Tags</p>
+					<ul>
+						{#each post.tags as postTag}
+							<li>
+								{data.tags.find((tag) => tag.id === postTag)?.name}
+							</li>
+						{/each}
+					</ul>
+				</footer>
+			</article>
+		</li>
+	{/each}
+</ul>
