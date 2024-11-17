e07d18083d000d93d5ab1af9e915afbba32706d4
Logan Woolf <logan.woolf@arcticleaf.io>
2024-11-17 15:07:22 -0700

    Posts page show only published posts

--- src/routes/posts/+page.server.ts
+++ src/routes/posts/+page.server.ts
@@ -2,7 +2,9 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals: { pb } }) => {
 	return {
-		posts: await pb.collection('posts').getList(1, 5),
+		posts: await pb.collection('posts').getList(1, 5, {
+			filter: pb.filter('published ~ true')
+		}),
 		tags: await pb.collection('tags').getFullList()
 	};
 };
