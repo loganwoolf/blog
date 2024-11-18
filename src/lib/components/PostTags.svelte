5b9d49dd8863971e3c977df16cf461b7f9de687d
Logan Woolf <loganwoolf@gmail.com>
2024-11-17 18:17:49 -0700

    Reformat tags

--- src/lib/components/PostTags.svelte
+++ src/lib/components/PostTags.svelte
@@ -2,41 +2,31 @@
 	let { post, tags } = $props();
 </script>
 
-<footer class="tags">
-	<p class="heading">Tags</p>
+<footer>
 	<ul>
 		{#each post.tags as postTag}
 			<li>
-				{tags.find((tag: { id: string; name: string }) => tag.id === postTag)?.name}
+				#{tags.find((tag: { id: string; name: string }) => tag.id === postTag)?.name}
 			</li>
 		{/each}
 	</ul>
 </footer>
 
 <style>
-	.tags {
+	footer {
 		border-top: 1px solid black;
 		margin-top: 1em;
 		padding-top: 1em;
+	}
 
-		.heading {
-			float: left;
-			margin: 0;
-		}
+	ul {
+		list-style: none;
+		margin: 0;
+		padding: 0;
+	}
 
-		ul {
-			list-style: none;
-			margin: 0;
-		}
-
-		li {
-			display: inline;
-
-			&:not(:last-child):after {
-				content: '';
-				border-right: 1px solid black;
-				margin: 0 0.5em;
-			}
-		}
+	li {
+		display: inline;
+		margin-right: 0.5em;
 	}
 </style>
