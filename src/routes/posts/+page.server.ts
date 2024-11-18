5bc1eb391fa6db735ea1ce97553791018eb570eb
Logan Woolf <loganwoolf@gmail.com>
2024-11-17 22:53:25 -0700

    Filter posts by tag name and sort descending by date

--- src/routes/posts/+page.server.ts
+++ src/routes/posts/+page.server.ts
@@ -1,9 +1,17 @@
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ locals: { pb } }) => {
+export const load: PageServerLoad = async ({ locals: { pb }, parent, url }) => {
+	const { tags } = await parent();
+
+	const tagId = tags.find((tag) => tag.name === url.searchParams.get('tag'))?.id;
+	const filter = tagId
+		? pb.filter('published = true && tags ~ {:tagId}', { tagId })
+		: pb.filter('published = true');
+
 	return {
 		posts: await pb.collection('posts').getList(1, 5, {
-			filter: pb.filter('published ~ true')
+			sort: '-publish_date',
+			filter
 		})
 	};
 };
