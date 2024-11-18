2df48929ecb6a628e7c5a1d1cba61c62f77833ee
Logan Woolf <loganwoolf@gmail.com>
2024-11-17 22:52:20 -0700

    Make tags into links

--- src/lib/components/PostTags.svelte
+++ src/lib/components/PostTags.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
 	let { post, tags } = $props();
+
+	const postTags = $derived(
+		post.tags.map(
+			(postTag: string) =>
+				tags.find((tag: { id: string; name: string }) => tag.id === postTag)?.name
+		)
+	);
 </script>
 
 <footer>
 	<ul>
-		{#each post.tags as postTag}
+		{#each postTags as postTag}
 			<li>
-				#{tags.find((tag: { id: string; name: string }) => tag.id === postTag)?.name}
+				<a href="/posts?tag={postTag}">
+					#{postTag}
+				</a>
 			</li>
 		{/each}
 	</ul>
@@ -28,5 +37,9 @@
 	li {
 		display: inline;
 		margin-right: 0.5em;
+	}
+
+	a:not(:hover) {
+		text-decoration: none;
 	}
 </style>
