1cacdf410b30a9874882bbde4e8a3dbb508c41d1
Logan Woolf <loganwoolf@gmail.com>
2024-11-20 23:41:46 -0700

    Set up deploy via node

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,13 @@
 import adapter from '@sveltejs/adapter-auto';
+import nodeAdapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import 'dotenv/config';
+
+const adapters = {
+	auto: () => adapter(),
+	netlify: () => adapter(),
+	node: () => nodeAdapter(),
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +19,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapters[process.env.ADAPTER || 'auto'](),
 	}
 };
 
