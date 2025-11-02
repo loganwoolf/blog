8002df7be6c6297be7e5625b0103017bdb85d5a2
Logan Woolf <logan.woolf@arcticleaf.io>
2025-11-01 20:35:21 -0600

    Resume skills section refactor content object

--- src/routes/resume/Skills.svelte
+++ src/routes/resume/Skills.svelte
@@ -1,38 +1,23 @@
 <script lang="ts">
 	import Section from './Section.svelte';
 
-	const skills: Array<{
-		segment: string;
-		content: string;
-	}> = [
-		{
-			segment: 'Languages and Frameworks',
-			content:
-				'NextJS, SvelteKit, JavaScript/TypeScript (including vanilla solutions), React, Svelte, Handlebars, CSS/SCSS, Tailwind, Strapi CMS, Postgres, SQLite.'
-		},
-		{
-			segment: 'E-commerce',
-			content:
-				'BigCommerce (Stencil themes, REST & GraphQL APIs, storefront management, B2B Edition and APIs, platform app development), Shopify (Shopify 2.0 themes, storefront management), Google Tag Manager, Klaviyo.'
-		},
-		{
-			segment: 'DevOps & Tools',
-			content:
-				'Git and GitHub, Linux and self-hosting, Netlify, Vercel, npm registry and library creation, CLI tool creation, Figma'
-		},
-		{
-			segment: 'Other',
-			content: 'Web accessibility, Pagespeed analysis'
-		}
-	];
+	const skills: Record<string, string> = {
+		'Languages and Frameworks':
+			'NextJS, SvelteKit, JavaScript/TypeScript (including vanilla solutions), React, Svelte, Handlebars, CSS/SCSS, Tailwind, Strapi CMS, Postgres, SQLite.',
+		'E-commerce':
+			'BigCommerce (Stencil themes, REST & GraphQL APIs, storefront management, B2B Edition and APIs, platform app development), Shopify (Shopify 2.0 themes, storefront management), Google Tag Manager, Klaviyo.',
+		'DevOps & Tools':
+			'Git and GitHub, Linux and self-hosting, Netlify, Vercel, npm registry and library creation, CLI tool creation, Figma',
+		Other: 'Web accessibility, Pagespeed analysis',
+	};
 </script>
 
-<Section heading="Skills">
+<Section heading="technical skills">
 	<ul>
-		{#each skills as skill (skill.segment)}
+		{#each Object.entries(skills) as [segment, content] (segment)}
 			<li>
-				<strong>{skill.segment}:</strong>
-				{skill.content}
+				<strong>{segment}:</strong>
+				{content}
 			</li>
 		{/each}
 	</ul>
